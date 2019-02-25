9a45d902d727f1cf767f54e4fb26e6ef0eb6636c
Giang Pham <conmeobeo96@gmail.com>
2019-02-25 15:43:48 +0700

    Add totalResults field to remote model for future pagination use

--- remote/src/main/java/leo/me/la/remote/model/RemoteMovieSearchModel.kt
+++ remote/src/main/java/leo/me/la/remote/model/RemoteMovieSearchModel.kt
@@ -5,5 +5,6 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class RemoteMovieSearchModel(
-    @Json(name = "Search") val result: List<RemoteMovieModel>
+    @Json(name = "Search") val result: List<RemoteMovieModel>,
+    val totalResults: Int
 )

b9fdeb8ad5baf84be83e0b202763996d858c732b
Giang Pham <conmeobeo96@gmail.com>
2019-03-01 08:50:36 +0700

    Rename main activity to search movies activity

--- app/src/main/java/leo/me/la/movies/SearchMoviesActivity.kt
+++ app/src/main/java/leo/me/la/movies/SearchMoviesActivity.kt
@@ -3,10 +3,10 @@ package leo.me.la.movies
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SearchMoviesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_search_movies)
     }
 }

11e934310f736cd891f4672a0f026f870a0266dd
Giang Pham <conmeobeo96@gmail.com>
2019-03-02 14:08:11 +0700

    Specify binding type

--- remote/src/main/java/leo/me/la/remote/RemoteModule.kt
+++ remote/src/main/java/leo/me/la/remote/RemoteModule.kt
@@ -7,6 +7,7 @@ import leo.me.la.common.TAG_INTERCEPTOR_LOGGING
 import leo.me.la.common.TAG_OMDB_API_KEY
 import leo.me.la.data.source.MovieRemoteDataSource
 import leo.me.la.remote.adapter.MovieSearchAdapter
+import okhttp3.Interceptor
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module.module
 
@@ -38,14 +39,14 @@ val remoteModule = module {
         )
     }
 
-    factory(name = TAG_INTERCEPTOR_LOGGING) {
+    factory<Interceptor>(name = TAG_INTERCEPTOR_LOGGING) {
         val isDebug: Boolean = get(name = TAG_BOOLEAN_DEBUG)
         HttpLoggingInterceptor().apply {
             level = if (isDebug) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
         }
     }
 
-    factory(name = TAG_INTERCEPTOR_API_KEY) {
+    factory<Interceptor>(name = TAG_INTERCEPTOR_API_KEY) {
         ApiKeyInterceptor(get(name = TAG_OMDB_API_KEY))
     }
 }

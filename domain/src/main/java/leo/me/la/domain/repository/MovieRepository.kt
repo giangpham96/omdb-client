60ee453c31a9b4b2ec9088b384a4bbbf061cf49f
Giang Pham <conmeobeo96@gmail.com>
2019-03-12 19:39:06 +0200

    Format code

--- domain/src/main/java/leo/me/la/domain/repository/MovieRepository.kt
+++ domain/src/main/java/leo/me/la/domain/repository/MovieRepository.kt
@@ -3,5 +3,5 @@ package leo.me.la.domain.repository
 import leo.me.la.common.model.MovieSearchResult
 
 interface MovieRepository {
-    suspend fun searchMoviesByKeyword(keyword: String, page: Int) : MovieSearchResult
+    suspend fun searchMoviesByKeyword(keyword: String, page: Int): MovieSearchResult
 }

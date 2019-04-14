dddd61eefb5d598d295f62f38e9f066466ff6e0f
Giang Pham <conmeobeo96@gmail.com>
2019-04-14 21:59:53 +0300

    Only make status bar translucent

--- app/src/main/java/leo/me/la/movies/MovieInfoActivity.kt
+++ app/src/main/java/leo/me/la/movies/MovieInfoActivity.kt
@@ -42,13 +42,6 @@ internal class MovieInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movie_info)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-            )
-        }
 
         val movieIds =
             intent?.extras?.getParcelableArrayList<ParcelableMovie>(MOVIES)

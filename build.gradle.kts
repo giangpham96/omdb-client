6fc474ff313eea10838b4fa1dd1c032f67b0c26f
Giang Pham <giang.pham@wolt.com>
2023-01-14 01:19:02 +0200

    Fix pr check and print test results

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+import org.gradle.api.tasks.testing.logging.TestLogEvent.FAILED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.PASSED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.SKIPPED
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_ERROR
+import org.gradle.api.tasks.testing.logging.TestLogEvent.STANDARD_OUT
+
 buildscript {
     repositories {
         google()
@@ -15,6 +22,16 @@ allprojects {
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
+    }
+
+    tasks.withType(Test::class.java) {
+        testLogging {
+            events = setOf(FAILED, PASSED, SKIPPED, STANDARD_ERROR, STANDARD_OUT)
+            exceptionFormat = FULL
+            showExceptions = true
+            showCauses = true
+            showStackTraces = true
+        }
     }
 }
 
